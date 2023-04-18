ceffff3d02a700a6d3c0e4d941b745b7d95d2600
Oicanji <51388779+Oicanji@users.noreply.github.com>
2023-04-17 21:50:37 -0300

    init frontend

--- tiermatch/main/views.py
+++ tiermatch/main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+#import Question by model
+
 @login_required
 def index(request):
     return render(request, 'main/home.html', {})
