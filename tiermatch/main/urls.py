731ddd403526016d8d58d70d55aed7d4e491468f
unknown <ignaciopunto102@outlook.com>
2023-04-22 16:07:18 -0300

    fix

--- tiermatch/main/urls.py
+++ tiermatch/main/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('quiz/', views.create_quiz, name='quiz'),
     path('play/', views.play, name='play'),
-    path('myquizzes/', views.my_quizzes, name='my quizzes'),
-    path('create_quiz/', views.create_quiz, name='create quiz'),
-
+    path('myquizzes/', views.my_quizzes, name='my_quizzes'),
+    
     path('category/create/', views.create_category, name='create_category'),
     path('category/get/', views.get_category, name='get_category'),
     path('category/remove/', views.remove_category, name='remove_category'),
