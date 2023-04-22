e13ac68301baf3b81ed941c04f3bed8cc0052a6a
RyanFrag <92130127+RyanFrag@users.noreply.github.com>
2023-04-22 17:39:04 -0300

    Update views.py

--- tiermatch/main/views.py
+++ tiermatch/main/views.py
@@ -24,22 +24,76 @@ def my_quizzes(request):
 def create_quiz(request):
     return render(request, 'pages/create_quiz.html', {})
 
+
+
 @login_required
 def play(request):
-    data = json.loads(request.body)
-    quiz_id = data.get('id')
-    if quiz_id:
-        res = []
-        quiz = Quiz.objects.filter(id=quiz_id).first()
-        if quiz:
-            res.append({"name": quiz.name, "description": quiz.description, "create_by_id": quiz.create_by_id, "create_at": quiz.create_at, 
-                        "super_allow_allias": quiz.super_allow_allias, "allow_allias": quiz.allow_allias, "deny_allias": quiz.deny_allias,
-                        "super_allow_color": quiz.super_allow_color, "allow_color": quiz.allow_color, "deny_color": quiz.deny_color})
-            return render(request, 'pages/play.html', { 'quiz': res })
-        else:
-            return render(request, 'pages/undefined.html', {})
-    else:
-        return render(request, 'pages/undefined.html', {})
+    return render(request, 'pages/play.html', {})
+#     if request.method != 'GET':
+#         return render(request, "pages/undefined.html")
+#     quiz_id = request.GET.get('id')
+#     quiz = Quiz.objects.filter(id=quiz_id).first()
+#     if quiz:
+#         res = {
+#             "id": quiz.id,
+#             "name": quiz.name,
+#             "description": quiz.description,
+#             "create_by_id": quiz.create_by_id.id,
+#             "create_by_name": quiz.create_by_id.username,
+#             "create_at": quiz.create_at.strftime("%d/%m/%Y %H:%M:%S"),
+#             "super_allow_allias": quiz.super_allow_allias,
+#             "allow_allias": quiz.allow_allias,
+#             "deny_allias": quiz.deny_allias,
+#             "super_allow_color": quiz.super_allow_color,
+#             "allow_color": quiz.allow_color,
+#             "deny_color": quiz.deny_color,
+#             "categories": [],
+#             "questions": [],
+#         }
+#         categories = Category.objects.filter(quiz_id=quiz.id)
+#         for category in categories:
+#             res['categories'].append({
+#                 "id": category.id,
+#                 "name": category.name,
+#                 "description": category.description,
+#                 "quiz_id": category.quiz_id.id,
+#                 "quiz_name": category.quiz_id.name,
+#                 "quiz_description": category.quiz_id.description,
+#                 "questions": [],
+#             })
+#             questions = Question.objects.filter(category_id=category.id)
+#             for question in questions:
+#                 res['questions'].append({
+#                     "id": question.id,
+#                     "name": question.name,
+#                     "description": question.description,
+#                     "category_id": question.category_id.id,
+#                     "category_name": question.category_id.name,
+#                     "category_description": question.category_id.description,
+#                     "quiz_id": question.category_id.quiz_id.id,
+#                     "quiz_name": question.category_id.quiz_id.name,
+#                     "quiz_description": question.category_id.quiz_id.description,
+#                     "alternatives": [],
+#                 })
+#                 alternatives = Alternative.objects.filter(question_id=question.id)
+#                 for alternative in alternatives:
+#                     res['questions'][-1]['alternatives'].append({
+#                         "id": alternative.id,
+#                         "name": alternative.name,
+#                         "description": alternative.description,
+#                         "question_id": alternative.question_id.id,
+#                         "question_name": alternative.question_id.name,
+#                         "question_description": alternative.question_id.description,
+#                         "category_id": alternative.question_id.category_id.id,
+#                         "category_name": alternative.question_id.category_id.name,
+#                         "category_description": alternative.question_id.category_id.description,
+#                         "quiz_id": alternative.question_id.category_id.quiz_id.id,
+#                         "quiz_name": alternative.question_id.category_id.quiz_id.name,
+#                         "quiz_description": alternative.question_id.category_id.quiz_id.description,
+#                     })
+#         return render(request, 'pages/play.html', res)
+#     return render(request, 'pages/undefined.html', {})
+
 
 @login_required
 def results(request):
@@ -141,6 +195,7 @@ def set_quiz(request):
     res.append({"name": quiz.name, "description": quiz.description, "create_by_id": quiz.create_by, "create_at": quiz.create_at, 
                     "super_allow_allias": quiz.super_allow_allias, "allow_allias": quiz.allow_allias, "deny_allias": quiz.deny_allias, 
                         "super_allow_color": quiz.super_allow_color, "allow_color": quiz.allow_color, "deny_color": quiz.deny_color})
+    print()
     args['response'] = res
     args['response'] = format_values(args)
     return response(200, args)
